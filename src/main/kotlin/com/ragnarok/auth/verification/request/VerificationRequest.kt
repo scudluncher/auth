5b1f3e1274308b78ad1c2622d81155258a2ea199
scudluncher <scudluncher@gmail.com>
2022-11-19 18:43:03 +0900

    불필요 annotation 제거

--- src/main/kotlin/com/ragnarok/auth/verification/request/VerificationRequest.kt
+++ src/main/kotlin/com/ragnarok/auth/verification/request/VerificationRequest.kt
@@ -1,13 +1,11 @@
 package com.ragnarok.auth.verification.request
 
-import com.fasterxml.jackson.annotation.JsonProperty
 import com.ragnarok.auth.verification.usecase.VerificationRequest
 import javax.validation.constraints.NotBlank
 import javax.validation.constraints.Pattern
 
 class VerificationRequest(
     @field:NotBlank
-    @field:JsonProperty
     @field:Pattern(regexp = "^[0-9]{11}", message ="ex) 01012349876 로 보내주세요" )
     private val phoneNumber: String
 ) {
